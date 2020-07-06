1979eb5982db4be4dab339cb121dff7e97f8150e
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-06 13:15:39 -0500

    finished multiplesDeleter

--- src/main/java/com/stayready/assessment1/part2/MultiplesDeleter.java
+++ src/main/java/com/stayready/assessment1/part2/MultiplesDeleter.java
@@ -1,4 +1,5 @@
 package com.stayready.assessment1.part2;
+import java.util.ArrayList;
 
 public class MultiplesDeleter {
     /**
@@ -7,7 +8,7 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with evens removed
      */
     public static Integer[] deleteEvens(Integer[] ints) {
-        return null;
+        return ansQuestions(ints, 2, 0);
     }
 
     /**
@@ -16,7 +17,7 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with odds removed
      */
     public static Integer[] deleteOdds(Integer[] ints) {
-        return null;
+        return ansQuestions(ints, 2, 1);
     }
 
     /**
@@ -25,7 +26,7 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by 3 removed
      */
     public static Integer[] deleteMultiplesOf3(Integer[] ints) {
-        return null;
+        return ansQuestions(ints, 3, 0);
     }
 
     /**
@@ -35,6 +36,25 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by `multiple` removed
      */
     public static Integer[] deleteMultiplesOfN(Integer[] ints, int multiple) {
-        return null;
+        return ansQuestions(ints, multiple, 0);
+    }
+
+    //refactor so that its less copy paste
+    public static Integer[] ansQuestions(Integer [] ints, int multiple, int check) {
+        ArrayList <Integer> arr = new ArrayList <Integer> ();
+        for(int element: ints) {
+            //all elements not divisible by multiple
+            if(element % multiple != check) {
+                arr.add(element);
+            }
+        }
+        Integer [] ans = new Integer [arr.size()];
+        int index = 0;
+        for(Integer el : arr) {
+            ans[index] = el;
+            index++;
+        }
+        //can't cast the array that arraylist can give to an Integer object, so had to create an Integer []
+        return ans;
     }
 }
