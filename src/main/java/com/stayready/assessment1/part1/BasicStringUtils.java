7b93a46b51240044476da37f3e088c3d05d7bb37
Junior Mitchell <spoony@macbook-pro.lan>
2020-07-06 18:59:38 -0400

    Assessment 1 for Review

--- src/main/java/com/stayready/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/stayready/assessment1/part1/BasicStringUtils.java
@@ -1,12 +1,17 @@
 package com.stayready.assessment1.part1;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class BasicStringUtils {
     /**
      * @param str string input from client
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        String output = Arrays.stream(str.split("\\s+")).map(t -> t.substring(0, 1).toUpperCase() + t.substring(1))
+                .collect(Collectors.joining(" "));
+        return output;
     }
 
     /**
@@ -14,32 +19,54 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+        return reverse;
     }
 
     /**
      * @param str string input from client
-     * @return string with identical contents, with each word individually in reverse order
+     * @return string with identical contents, with each word individually in
+     *         reverse order
      */
     public static String reverseWords(String str) {
-        return null;
+        String[] words = str.split(" ");
+        String reversedString = "";
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            String reverseWord = "";
+            for (int j = word.length() - 1; j >= 0; j--) {
+
+                reverseWord = reverseWord + word.charAt(j);
+            }
+            reversedString = reversedString + reverseWord + " ";
+        }
+        return reversedString.trim();
     }
 
     /**
      * @param str string input from client
-     * @return string with identical contents, in reverse order, with first character capitalized
+     * @return string with identical contents, in reverse order, with first
+     *         character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+        StringBuilder resultBuilder = new StringBuilder();
+        for (String string : str.split(" ")) {
+            String revStr = new StringBuilder(string).reverse().toString();
+            revStr = Character.toUpperCase(revStr.charAt(0)) + revStr.substring(1);
+            resultBuilder.append(revStr).append(" ");
+        }
+        return resultBuilder.toString();
     }
-
 
     /**
      * @param str a string input from user
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+        return str.substring(1, str.length() - 1);
     }
 
     /**
@@ -49,5 +76,4 @@ public class BasicStringUtils {
     public static String invertCasing(String str) {
         return null;
     }
-
 }
