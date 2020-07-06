19b6272b21ed10d87aa8a93f39e2f31b4915b626
toribreeana <tori.bree@outlook.com>
2020-07-06 19:10:38 -0400

    Ready for Review

--- src/main/java/com/stayready/assessment1/part1/BasicArrayUtils.java
+++ src/main/java/com/stayready/assessment1/part1/BasicArrayUtils.java
@@ -1,21 +1,44 @@
 package com.stayready.assessment1.part1;
 
+import java.util.ArrayList;
+
 public class BasicArrayUtils {
     /**
      * @param stringArray an array of String objects
      * @return the first element in the array
      */
-    public static String getFirstElement(String[] stringArray) {
-        return null;
+
+
+    public static void main(String[] args) {
+     // creating an Empty Integer ArrayList 
+
+        String [] words={"The", "quick", "brown"};
+        int index=0;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i] == "The") {
+            }
+        }
+        System.out.println("Index="+index);
+        System.out.println("The");
     }
 
-    /**
+        /**
      * @param stringArray an array of String objects
      * @return the second element in the array
      */
-    public static String getSecondElement(String[] stringArray) {
-        return null;
+
+    public static void main(String[] args) {
+       // creating an Empty Integer ArrayList 
+       
+    String words[] = {"Somewhere", "over", "the", "rainbow"};
+    System.out.println("Somewhere: "+words.length); 
+    for (int i = 0; i <words.length; i++);
+    {
+        int i;
+		System.out.println(words[i]+ " "); }
+        
     }
+
 
     /**
      * @param stringArray an array of String objects
