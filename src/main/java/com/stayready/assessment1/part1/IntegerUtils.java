b3bd7cb07f66e0bfe41d0d710de9ee54187e8b22
Dominique <cyanthecomputerscientist@gmail.com>
2020-07-06 17:09:34 -0400

    Finished classes

--- src/main/java/com/stayready/assessment1/part1/IntegerUtils.java
+++ src/main/java/com/stayready/assessment1/part1/IntegerUtils.java
@@ -6,7 +6,12 @@ public class IntegerUtils {
      * @return the sum of all integers between 0 and not including `n`
      */
     public static Integer getSumOfN(Integer n) {
-        return null;
+        int sum = 0;
+        for (int i = 0; i<n; i++)
+        {
+            sum +=i;
+        }
+        return sum;
     }
 
     /**
@@ -22,7 +27,16 @@ public class IntegerUtils {
      * @return true if the integer is even and false if it is not even.
      */
     public static Boolean isEven(Integer val) {
-        return null;
+        Boolean iseven = true;
+        if(val%2 ==0)
+        {
+            iseven = true;
+        }
+        else
+        {
+            iseven = false;
+        }
+        return iseven;
     }
 }
 
